b8aea4405eb57d9152e70834586d737b1e38bce2
Albert Dang <adang018@wch136-30.cs.ucr.edu>
2019-10-28 10:20:50 -0700

    Finished Lab8

---
+++ turnin/adang018_mwu057_lab8_part2.c
@@ -0,0 +1,36 @@
+/*	Author: Albert Dang adang018
+ *      Partner(s) Name: Min-Hua Wu mwu057
+ *	Lab Section: 022
+ *	Assignment: Lab #8  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned short output;
+// const unsigned short lightMax = 96;
+
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    ADC_init();
+
+    while (1) {
+        output = ADC;
+        PORTB = (char)output;
+        PORTD = (char)(output >> 8);
+        // PORTB = (output >= (lightMax / 2)) ? 1 : 0;
+    }
+    return 1;
+}
